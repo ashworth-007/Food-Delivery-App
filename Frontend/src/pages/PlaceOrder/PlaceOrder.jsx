4b4b8501b4ad7d78bd1de95532e3a0cf5cc645f4
ashworth-007 <137112086+ashworth-007@users.noreply.github.com>
2025-01-15 01:48:14 +0530

    added mock data

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -24,32 +24,72 @@ const PlaceOrder = () => {
     const value = event.target.value;
     setData(data=>({...data,[name]:value}))
   }
-  const placeOrder = async (event) => {
-    event.preventDefault();
-    let orderItems = [];
-    food_list.map((item)=>{
-      if (cartItems[item._id]>0) {
-        let itemInfo = item;
-        itemInfo["quantity"] = cartItems[item._id];
-        orderItems.push(itemInfo)
-      }
-    })
-    let orderData = {
-      address:data,
-      items:orderItems,
-      amount:getTotalCartAmount()+2,
-    }
-    let response = await axios.post(url+"/api/order/place",orderData,{headers:{token}})
+  // const placeOrder = async (event) => {
+  //   event.preventDefault();
+  //   let orderItems = [];
+  //   food_list.map((item)=>{
+  //     if (cartItems[item._id]>0) {
+  //       let itemInfo = item;
+  //       itemInfo["quantity"] = cartItems[item._id];
+  //       orderItems.push(itemInfo)
+  //     }
+  //   })
+  //   let orderData = {
+  //     address:data,
+  //     items:orderItems,
+  //     amount:getTotalCartAmount()+2,
+  //   }
+  //   let response = await axios.post(url+"/api/order/place",orderData,{headers:{token}})
     
 
-    if (response.data.success){
-      const {session_url} = response.data;
-      window.location.replace(session_url);
+  //   if (response.data.success){
+  //     const {session_url} = response.data;
+  //     window.location.replace(session_url);
+  //   }
+  //   else{
+  //     alert("Error");
+  //   }
+  // }
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    try {
+      let orderItems = [];
+      food_list.forEach((item) => {
+        if (cartItems[item._id] > 0) {
+          let itemInfo = { ...item, quantity: cartItems[item._id] };
+          orderItems.push(itemInfo);
+        }
+      });
+      let orderData = {
+        address: data,
+        items: orderItems,
+        amount: getTotalCartAmount() + 2,
+      };
+      console.log(orderData);
+      let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+      console.log(response);
+      if (response.data.success) {
+        const { session_url } = response.data;
+        window.location.replace(session_url);
+      } else {
+        alert("Error: Unable to place order");
+      }
+    } catch (error) {
+      console.error("Error placing order:", error);
+      if (error.response) {
+        console.error("Response Data:", error.response.data);
+        console.error("Status Code:", error.response.status);
+      } else if (error.request) {
+        console.error("Request:", error.request);
+      } else {
+        console.error("Error Message:", error.message);
+      }
+      alert("An error occurred while placing your order. Please try again.");
     }
-    else{
-      alert("Error");
-    }
-  }
+    
+  };
+  
 
   const navigate = useNavigate();
 
